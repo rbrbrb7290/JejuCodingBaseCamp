c94aeb91abd4a73c123c5bd7c025c69e7bb82cc7
rbrbrb7290 <ohg429@gmail.com>
2020-01-09 16:42:29 +0900

    youtube data api load success

--- components/LectureComponent/LectureIntro.js
+++ components/LectureComponent/LectureIntro.js
@@ -1,12 +1,51 @@
-import React from 'react';
-import { View, Text, Button, TouchableOpacity} from "react-native"
+import React, {useState, useEffect} from 'react';
+import { View, Text, ScrollView, FlatList, ActivityIndicator} from "react-native"
+import * as env from "../../env";
+
+async function _getPlayList() {
+    const YOUTUBE_API_KEY = env.YOUTUBE_API_KEY;
+    const playListId= env.PLAY_LIST_ID;
+
+    const url = `https://www.googleapis.com/youtube/v3/playlistItems?part=id,snippet&fields=items(id,snippet(title))&playlistId=${playListId}&key=${YOUTUBE_API_KEY}`
+    const options = {
+        method: 'GET',
+        headers: { 
+            'Accept': 'application/json',
+            'Content-Type' : 'application/json;charset=UTF-8'
+        },
+    }   
+    let res = await fetch(url , options);
+    let resOk = res && res.ok;
+
+    if(resOk) {
+        let data = await res.json();
+        return data;
+    }
+}
+
+
 
 const LectureIntro = () => {
+    const [playList, setPlayList] = useState(null);
+    
+    const getPlayList = async () => {
+        setPlayList(await _getPlayList());
+    }
 
-    return(
-      <View style={{ flex: 1 , justifyContent: 'center' , alignItems: 'center'}}>
-        <Text> 강의 개요 뷰 </Text>
-     </View>
+    useEffect(()=> {getPlayList()}, []);
+    console.log(playList)
+    
+    return !playList ? (
+        <View style={{alignItems: 'center', paddingTop:20, flex:1}}>
+            <ActivityIndicator size='large' />
+        </View> 
+    ) : (
+        <ScrollView>
+            <View>
+                <Text>리스트뿌려질예정</Text>
+            </View>   
+        </ScrollView>
+      
     );
 } 
 
