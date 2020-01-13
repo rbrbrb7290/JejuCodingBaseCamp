f4fa66099c7878d00acb84a6bb2c925b05ed8fea
wonbeomchoi <cwb2310@gmail.com>
2020-01-13 11:27:03 +0900

    admob test

--- components/Home.js
+++ components/Home.js
@@ -1,11 +1,16 @@
-import React from 'react';
+import React, { Fragment } from 'react';
 import { View, Text, Button, TouchableOpacity} from "react-native"
+import Admob from './Admob';
 
 const Home = () => {
     return(
-      <View style={{ flex: 1 , justifyContent: 'center' , alignItems: 'center'}}>
-        <Text>Home</Text>
-     </View>
+      <Fragment>
+
+        <View style={{ flex: 1 , justifyContent: 'center' , alignItems: 'center'}}>
+          <Text>Home</Text>
+      </View>
+      <Admob/>
+      </Fragment>
     ); 
 } 
 
