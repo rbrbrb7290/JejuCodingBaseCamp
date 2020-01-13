12634c3700b8966406b0c5d1c14224a1f5c060ac
rbrbrb7290 <ohg429@gmail.com>
2020-01-13 11:59:46 +0900

    data processing

--- components/LectureComponent/LectureIntro.js
+++ components/LectureComponent/LectureIntro.js
@@ -1,38 +1,36 @@
 import React, {useState, useEffect} from 'react';
 import { View, Text, ScrollView, FlatList, ActivityIndicator} from "react-native"
-import * as env from "../../env";
+import { getPlayList } from '../../service/DataProcessor';
 
-async function _getPlayList() {
-    const YOUTUBE_API_KEY = env.YOUTUBE_API_KEY;
-    const playListId= env.PLAY_LIST_ID;
+// async function _getPlayList() {
+//     const YOUTUBE_API_KEY = env.YOUTUBE_API_KEY;
+//     const playListId= env.PLAY_LIST_ID;
 
-    const url = `https://www.googleapis.com/youtube/v3/playlistItems?part=id,snippet&fields=items(id,snippet(title))&playlistId=${playListId}&key=${YOUTUBE_API_KEY}`
-    const options = {
-        method: 'GET',
-        headers: { 
-            'Accept': 'application/json',
-            'Content-Type' : 'application/json;charset=UTF-8'
-        },
-    }   
-    let res = await fetch(url , options);
-    let resOk = res && res.ok;
+//     const url = `https://www.googleapis.com/youtube/v3/playlistItems?part=id,snippet&fields=items(id,snippet(title))&playlistId=${playListId}&key=${YOUTUBE_API_KEY}`
+//     const options = {
+//         method: 'GET',
+//         headers: { 
+//             'Accept': 'application/json',
+//             'Content-Type' : 'application/json;charset=UTF-8'
+//         },
+//     }   
+//     let res = await fetch(url , options);
+//     let resOk = res && res.ok;
 
-    if(resOk) {
-        let data = await res.json();
-        return data;
-    }
-}
+//     if(resOk) {
+//         let data = await res.json();
+//         return data;
+//     }
+// }
 
-
-
-const LectureIntro = () => {
+function LectureIntro() {
     const [playList, setPlayList] = useState(null);
     
-    const getPlayList = async () => {
-        setPlayList(await _getPlayList());
+    const _getPlayList = async () => {
+        setPlayList(await getPlayList());
     }
 
-    useEffect(()=> {getPlayList()}, []);
+    useEffect(()=> {_getPlayList()}, []);
     console.log(playList)
     
     return !playList ? (
---
+++ service/DataProcessor.js
@@ -0,0 +1,22 @@
+import  _getPlayList  from '../lib/YouTubeAPI';
+
+async function getPlayList() {
+        let res;
+        console.log('start')
+        try {
+            res = await _getPlayList();
+        } catch (error) {
+            res = await _getPlayList();
+        }finally{
+            if(!res) return res;
+            return res['items'].map(row => ({
+                title: row['snippet']['title'],
+                desc: row['snippet']['description'],
+                img: row['snippet']['thumbnails']['default']['url'],
+                date: row['snippet']['publishedAt'],
+                videoId: row['contentDetails']['videoId']
+            }));
+        }
+}
+
+export { getPlayList }
