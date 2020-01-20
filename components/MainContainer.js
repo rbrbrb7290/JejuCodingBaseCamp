2be61677148bba65190d83994825a0180b5d0b65
wonbeomchoi <cwb2310@gmail.com>
2020-01-20 16:48:18 +0900

    clean lib + add license

--- components/MainContainer.js
+++ components/MainContainer.js
@@ -4,9 +4,10 @@ import { createAppContainer } from 'react-navigation';
 import { createStackNavigator } from 'react-navigation-stack';
 import Icon from 'react-native-vector-icons/Entypo';
 import LectureDrawerNavigator from './LectureDrawerNavigator';
+import License from './License'
 
 const icon = <Icon name="menu" size={32}/>
-
+const infoIcon = <Icon name="info" size={20}/>
 const HomeStackNavigator = createStackNavigator({
     Home: {
         screen: LectureDrawerNavigator,
@@ -15,7 +16,7 @@ const HomeStackNavigator = createStackNavigator({
             headerTitle: ()=> (
               <View>
                 <Image source={require('./../images/logo.png')}
-                       style={{width:40 , height:40, marginLeft: -32}} /> 
+                       style={{width:40 , height:40, marginLeft: -32}} />
               </View>
             ),
             headerLeft: () => ( 
@@ -23,9 +24,16 @@ const HomeStackNavigator = createStackNavigator({
                 {icon}
               </TouchableOpacity> 
             ),
-          
-          })    
+            headerRight:()=>(
+              <TouchableOpacity style={{marginRight:10}} onPress={()=> navigation.navigate('License')}>
+              <Icon name="info" size={20} style={{marginLeft:100}}/>
+              </TouchableOpacity>
+            )
+          })
     },
+    License:{
+      screen: License
+    }
 })
 
 export default createAppContainer(HomeStackNavigator);
