4df87800085e9ac1bdf75f86e982b924d952f8ca
rbrbrb7290 <ohg429@gmail.com>
2020-02-03 20:21:31 +0900

    ios version update

--- components/MainContainer.js
+++ components/MainContainer.js
@@ -1,5 +1,5 @@
 import React from 'react'; 
-import { TouchableOpacity, Image, View, Text} from 'react-native';
+import { TouchableOpacity, Image, View, Text, Platform, StyleSheet} from 'react-native';
 import { createAppContainer } from 'react-navigation';
 import { createStackNavigator } from 'react-navigation-stack';
 import Icon from 'react-native-vector-icons/Entypo';
@@ -15,23 +15,23 @@ const HomeStackNavigator = createStackNavigator({
     Home: {
         screen: LectureDrawerNavigator,
         navigationOptions: ({navigation}) => ({
-            title: 'PaulLab',
+          title: "",
             headerTitle: ()=> (
               <View>
                 <Image source={require('./../images/logo.png')}
-                       style={{width:40 , height:40, marginLeft: -20}} /> 
+                       style={style.headerTitle} /> 
                </View>
             ),
             headerLeft: () => (   
               <TouchableOpacity
                   onPress={() => navigation.toggleDrawer()} 
-                  style={{marginLeft:5}}>
+                  style={style.headerLeft}>
                     {icon}
               </TouchableOpacity> 
             ),
             headerRight:()=>(
               <TouchableOpacity
-                  style={{marginRight:-15,height:48, width:48, alignContent:'center',justifyContent:'center'}} 
+                  style={style.headerRight} 
                   onPress={()=> navigation.navigate('License')}>
                     {infoIcon}
               </TouchableOpacity>
@@ -59,4 +59,28 @@ const HomeStackNavigator = createStackNavigator({
     }
 })
 
+const style = StyleSheet.create({
+  headerTitle: {
+        width: 40,
+        height: 40,
+        ...Platform.select({
+          ios: {
+            marginLeft: -150 ,
+          },
+          android:{
+            marginLeft: -20
+          }
+        })
+  },
+  headerLeft:{
+    marginLeft:5
+  },
+  headerRight:{
+    marginRight:-15,
+    height:48, 
+    width:48, 
+    alignContent:'center',
+    justifyContent:'center'
+  }
+})
 export default createAppContainer(HomeStackNavigator);
